5be5de1ad583099648721d7f48235e968fcb446b
David Burles <dburles@gmail.com>
2019-04-23 15:55:59 +1000

    mode shortcut keys

--- src/components/Transport.container.js
+++ src/components/Transport.container.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import Tone from 'tone';
+import KeyboardEventHandler from 'react-keyboard-event-handler';
 import Transport from './Transport';
+
+const modeKeyMap = {
+  p: 'prf',
+  '[': 'seq',
+  ']': 'pat',
+};
 
 const TransportContainer = ({ state, dispatch, togglePlay }) => {
   return (
-    <Transport
-      onChangeMode={mode => {
-        dispatch({ type: 'mode', mode });
-      }}
-      mode={state.mode}
-      bpm={state.bpm}
-      onTogglePlay={() => {
-        togglePlay();
-      }}
-      onChangeSwing={event => {
-        dispatch({ type: 'swing', swing: event.target.value });
-      }}
-      onChangeBpm={event => {
-        dispatch({ type: 'bpm', bpm: event.target.value });
-      }}
-      swing={state.swing}
-    />
+    <>
+      <KeyboardEventHandler
+        handleKeys={['p', '[', ']']}
+        onKeyEvent={(key, event) => {
+          dispatch({ type: 'mode', mode: modeKeyMap[key] });
+        }}
+      />
+      <Transport
+        onChangeMode={mode => {
+          dispatch({ type: 'mode', mode });
+        }}
+        mode={state.mode}
+        bpm={state.bpm}
+        onTogglePlay={() => {
+          togglePlay();
+        }}
+        onChangeSwing={event => {
+          dispatch({ type: 'swing', swing: event.target.value });
+        }}
+        onChangeBpm={event => {
+          dispatch({ type: 'bpm', bpm: event.target.value });
+        }}
+        swing={state.swing}
+      />
+    </>
   );
 };
 
