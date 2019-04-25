198127ce158628e1e658514f430696ea32efb8ca
David Burles <dburles@gmail.com>
2019-04-25 20:33:12 +1000

    no bg

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -4,7 +4,7 @@ import { themeGet } from 'styled-system';
 const GlobalStyle = createGlobalStyle`
   body {
     background-color: ${themeGet('colors.darkGray')};
-    background-image: url('https://source.unsplash.com/random');
+    /*background-image: url('https://source.unsplash.com/random');*/
     background-size: cover;
     font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;
   }
