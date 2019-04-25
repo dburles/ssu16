6570769ec4fcfb8f7a8c4a88e75678803c0d8bbf
David Burles <dburles@gmail.com>
2019-04-25 14:11:17 +1000

    background

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,7 +2,7 @@ import { Flex } from 'rebass';
 import styled from 'styled-components';
 
 const Container = styled(Flex)`
-  /*background: rgba(0, 0, 0, 0.5);*/
+  background: rgba(0, 0, 0, 0.5);
 `;
 
 export default Container;
