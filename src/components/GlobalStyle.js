903a5018f6460cad8c4fdf6ee1c9f9a777a3b1c2
David Burles <dburles@gmail.com>
2019-05-17 16:28:11 +1000

    no wp

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -1,6 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
 import { themeGet } from 'styled-system';
-import wallpaper from '../wallpapers/glass.jpg';
 
 // background-image: url('https://source.unsplash.com/random');
 // background-image: url(${wallpaper});
@@ -9,7 +8,6 @@ const GlobalStyle = createGlobalStyle`
   body {
     background-color: ${themeGet('colors.darkGray')};
     background-size: cover;
-    background-image: url(${wallpaper});
     font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;
   }
   button {
